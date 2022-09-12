b26742adbde1b51d05fc04dd807425dc5635e971
rgongw <a1055547@mail.nuk.edu.tw>
2022-09-13 04:11:24 +0800

    add CPE probplems

---
+++ CPE/108 - Maximum Sum/main.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+#define ll long long 
+#define pb push_back
+#define endl '\n'
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(NULL);
+    ll n, m;
+    vector<int> rec, indices;
+    while (cin >> n){
+        if (n == 0) break;
+        int e;
+        rec.reserve(n*n);
+        indices.reserve(n*n);
+        while (m--) {
+            cin >> e;
+            rec.pb(e);
+        }
+
+        ll sum = 0, max = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (sum + rec[i*n + j] > sum){
+                    sum = sum + rec[i*n + j];
+
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cout << rec[i*n + j] << " ";
+            }
+            cout << endl;
+        }
+
+        // int rst = 0;
+        // for (auto &i: rec){
+        //     if (rst == n) {
+        //         cout << endl;
+        //         rst = 0;
+        //     }
+        //     cout << i << " ";
+        //     rst++;
+        // }
+        cout << endl;
+        rec.clear();
+    }
+}
