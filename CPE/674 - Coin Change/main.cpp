b59bc739765ec7697e6bb50629546084a9b4f174
rgongw <a1055547@mail.nuk.edu.tw>
2022-09-15 00:31:57 +0800

    done

--- CPE/674 - Coin Change/main.cpp
+++ CPE/674 - Coin Change/main.cpp
@@ -9,25 +9,23 @@ int main() {
     int c[5] = {1, 5, 10, 25, 50};
     ios::sync_with_stdio(0);
     cin.tie(NULL);
+    const int MAX_VALUE = 7489;
+    vector<int> ways(MAX_VALUE, 0);
+    ways[0] = 1;
+    for (int i = 0; i < 5; i++) {
+        for (int v = 1; v <= MAX_VALUE; v++) {
+            if (v - c[i] >= 0) {
+                ways[v] += ways[v - c[i]];
+            }
+        }
+    }
     int n;
     while (cin >> n) {
         if (n <= 0) {
             cout << 1 << endl;
         }
         else {
-            int N = n;
-            vector<int> dp(n, 0);
-            for (int i = 0; i < 5; i++) {
-                for (int v = 0; v <= n; v++) {
-                    for (int k = 0; k < n / c[i]; k++) {
-                        if (v - k * c[i] >= 0) {
-                            dp[v]
-
-                        }
-                    }
-                }
-
-            }
+            cout << ways[n] << endl;
         }
     }
 }
