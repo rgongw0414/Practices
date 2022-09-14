813bc985b7582d2f63b647ce587cee258910ea69
rgongw0414 <smileeric0414@gmail.com>
2022-09-14 15:41:40 +0800

    new

---
+++ CPE/674 - Coin Change/main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define eb emplace_back
+#define endl '\n'
+using namespace std;
+
+int main() {
+    // 50, 25, 10, 5, 1-cent
+    ios::sync_with_stdio(0);
+    cin.tie(NULL);
+    int n;
+    while (cin >> n) {
+        if (n <= 0) {
+            cout << 0 << endl;
+        }
+        else {
+            
+        }
+    }
+}
