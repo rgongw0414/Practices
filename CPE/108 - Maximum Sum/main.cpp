3d644a28f01846b5153b33454d04e5665961df32
rgongw <smileeric0414@gmail.com>
2022-09-14 15:17:17 +0800

    done

--- CPE/108 - Maximum Sum/main.cpp
+++ CPE/108 - Maximum Sum/main.cpp
@@ -5,17 +5,26 @@
 using namespace std;
 
 int max1d(vector<int> &rec){
-    return 0;
+    int maxSum = INT_MIN, sum = 0;
+    for (int i = 0; i < rec.size(); i++){
+        if (sum >= 0) {
+            sum = sum + rec[i];
+        }
+        else {
+            sum = 0;
+        }
+        if (sum > maxSum) maxSum = sum;
+    }
+    return maxSum;
 }
 
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(NULL);
     int n;
-    cout << INT_MIN << endl;
     while (cin >> n) {
         if (n == 0) break;
-        int max = INT_MIN;
+        int maxSum = INT_MIN;
         vector<vector<int>> rec(n, vector<int>(n, 0));
         for (auto& row: rec){
             for(auto& elem: row){
@@ -26,12 +35,12 @@ int main(){
         for (int i = 0; i < n; i++) {
             vector<int> sum(n, 0);
             for (int j = i; j < n; j++) {
-                // cout << rec[i][j] << " ";
                 for (int k = 0; k < n; k++){
-
+                    sum[k] = sum[k] + rec[j][k];
                 }
+                maxSum = max(maxSum, max1d(sum));
             }
-            cout << endl;
         }
+        cout << maxSum << endl;
     }
 }
