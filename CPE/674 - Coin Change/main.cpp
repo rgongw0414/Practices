6a2790e2898fcb45372880e3413546d88177a853
rgongw0414 <smileeric0414@gmail.com>
2022-09-14 17:44:28 +0800

    new

--- CPE/674 - Coin Change/main.cpp
+++ CPE/674 - Coin Change/main.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 int main() {
     // 50, 25, 10, 5, 1-cent
+    int c[5] = {1, 5, 10, 25, 50};
     ios::sync_with_stdio(0);
     cin.tie(NULL);
     int n;
@@ -14,7 +15,18 @@ int main() {
             cout << 0 << endl;
         }
         else {
-            
+            int N = n;
+            vector<int> dp(n, 0);
+            for (int i = 0; i < 5; i++) {
+                for (int v = 0; v <= n; v++) {
+                    for (int k = 0; k < n / c[i]; k++) {
+                        if (v - k * c[i] >= 0) {
+                            
+                        }
+                        dp[v]
+                    }
+                }
+            }
         }
     }
 }
