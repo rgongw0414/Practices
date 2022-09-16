5df65f549f8ad18d4a3b45e0c38d4783a84cbde4
rgongw <a1055547@mail.nuk.edu.tw>
2022-09-16 12:08:26 +0800

    new

---
+++ CPE/11360 - Having Fun with Matrices/main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define em emplace_back
+#define endl '\n'
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(NULL);
+
+    
+}
