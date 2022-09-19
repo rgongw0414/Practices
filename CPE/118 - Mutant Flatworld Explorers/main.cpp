2dce8462be68292c63c54a386796d578ed466f6c
rgongw <a1055547@mail.nuk.edu.tw>
2022-09-19 16:19:19 +0800

    new

---
+++ CPE/118 - Mutant Flatworld Explorers/main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define endl '\n'
+#define eb emplace_back
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(NULL);
+
+    
+}
