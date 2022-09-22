9ff60d37bbe9c3807a89ea75bd0623205c0efbbf
rgongw <a1055547@mail.nuk.edu.tw>
2022-09-23 00:10:45 +0800

    AC

--- CPE/12959 Strategy Game/main.cpp
+++ CPE/12959 Strategy Game/main.cpp
@@ -8,5 +8,21 @@ int main() {
     ios::sync_with_stdio(0);
     cin.tie(NULL);
 
-    
+    int j, r, n;
+    while (cin >> j >> r) {
+        if (j == 0 && r == 0) break;
+        vector<int> vp(j, 0);
+        // n = j * r;
+        while (r--) {
+            for (auto& p: vp) {
+                cin >> n;
+                p += n;
+            }
+        }
+        int max = 0;
+        for (int i = 1; i < j; i++) {
+            if (vp[i] >= vp[max]) max = i;
+        }
+        cout << max + 1 << endl;
+    }
 }
