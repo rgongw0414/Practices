3be8b4ce7d967004a25b3574042921dd64e9651d
rgongw <a1055547@mail.nuk.edu.tw>
2022-12-04 01:24:23 +0800

    AC

--- CPE/11349 Symmetric Matrix/main.cpp
+++ CPE/11349 Symmetric Matrix/main.cpp
@@ -37,7 +37,7 @@ int main() {
             continue;
         }
         for (int i = 0; i <= n/2; i++) {
-            for (int j = 0; j < n; j++) {
+            for (int j = 0; j < n-i; j++) {
                 if (mat[i][j] != mat[n-1-i][n-1-j]) sym = false;
             }
         }
