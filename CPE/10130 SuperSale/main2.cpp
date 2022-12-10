e60939b6f528740a3566b58d3d5c70f44221f8fd
rgongw <a1055547@mail.nuk.edu.tw>
2022-12-10 22:40:04 +0800

    AC

--- CPE/10130 SuperSale/main2.cpp
+++ CPE/10130 SuperSale/main2.cpp
@@ -11,15 +11,15 @@ vector<int> weights;
 
 int solve(int n, int w) {
     vector<vector<int>> c(n+1, vector<int>(w+1, 0));
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j <= w; j++) {
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= w; j++) {
             if (j - weights[i] < 0) {
-                c[i+1][j] = c[i][j];
+                c[i][j] = c[i-1][j];
             }
             else {
-                c[i+1][j] = max(
-                    c[i][j], 
-                    c[i][j-weights[i]] + prices[i]
+                c[i][j] = max(
+                    c[i-1][j], 
+                    c[i-1][j-weights[i]] + prices[i]
                 );
             }
         }
@@ -27,16 +27,29 @@ int solve(int n, int w) {
     return c[n][w];
 }
 
+int solve2(int n, int w) {
+    if (n == 0) 
+        return 0;
+    if (n > 0 && w < 0) 
+        return solve2(n-1, w);
+    
+    return max(
+        solve2(n-1, w), 
+        solve2(n-1, w-weights[n]) + prices[n]
+        );
+}
+
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(NULL);
-    
+
     int t;
     cin >> t;
     while (t--) {
         int n;
         cin >> n;
-        prices.reserve(n); weights.reserve(n);
+        prices.reserve(n+1); weights.reserve(n+1);
+        prices.eb(0); weights.eb(0);
         int tmp = n;
         while (n--) {
             int p, w;
@@ -51,7 +64,7 @@ int main() {
         while (p--) {
             int maxW;
             cin >> maxW;
-            ans += solve(n, maxW);
+            ans += solve2(n, maxW);
         }
         cout << ans << endl;
         prices.clear(); weights.clear();
