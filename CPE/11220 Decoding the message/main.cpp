03892c90416c380d671e7cf2848fb3b789459e36
rgongw <a1055547@mail.nuk.edu.tw>
2023-01-13 04:23:41 +0800

    AC

--- CPE/11220 Decoding the message/main.cpp
+++ CPE/11220 Decoding the message/main.cpp
@@ -5,17 +5,16 @@ int main() {
 	cin >> t;
 	cin.get(); // catch '\0' from the first line
 	cin.get(); // catch second line, i.e. "\0"
-	while(t--) {
+	while (t--) {
 		string s, ans;
 		stringstream ss;
-		printf("Case #%d:\n", c++);
-		while(getline(cin, s) && s != "") {
-			ss.str("");
+		printf ("Case #%d:\n", c++);
+		while (getline(cin, s) && s != "") {
 			ss.clear();
 			int n = 0;
 			ss << s;
-			while(ss >> ans) {
-				if(ans.size() > n) {
+			while (ss >> ans) {
+				if (ans.size() > n) {
 					cout << ans[n];
 					n++;
 				}
