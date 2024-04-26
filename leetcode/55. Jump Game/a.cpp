fa82dd8e92a5ad8c9f50a825d26c324f65b44a86
eric <smileeric0414@gmail.com>
2024-04-27 02:57:33 +0800

    update

--- leetcode/55. Jump Game/a.cpp
+++ leetcode/55. Jump Game/a.cpp
@@ -1,7 +1,40 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        // brute-force: O(N^2) for TC
+        // greedy with TC of O(N), in a way similar to Kadane's algorithm
+        // dynamically updates reachable position, pushing it from head to tail
+        int currReachable = 0;
+        for (int curr_pos = 0; curr_pos < nums.size(); curr_pos++) {
+            if (currReachable < curr_pos || currReachable >= nums.size() - 1) {
+                // curr_pos can't be reached, meaning that we can't not go any further
+                break;
+            }
+            currReachable = max(currReachable, curr_pos + nums[curr_pos]);
+        }
+        return (currReachable >= nums.size() - 1);
+    }
+};
+
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        // greedy with TC of O(N), note that using size_t as index type in for loop will cause overflowing.
+        // dynamically updates goal position, pushing it from tail to head
+        int goal = nums.size() - 1;
+        for (int curr_pos = nums.size() - 1; curr_pos >= 0; curr_pos--) {
+            // size_t will cause curr_pos overflow to UINT_MAX in the last iteration of for loop (last iter: 0 - 1 = UINT_MAX)
+            if (curr_pos + nums[curr_pos] >= goal) goal = curr_pos;
+        }
+        bool reachable = (goal == 0);
+        return reachable;
+    }
+};
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        // brute-force: O(N^N) for TC
         vector<bool> reachable(nums.size(), false);
         reachable[0] = true;
 
