38ce386fa4ef538076760432063cedd54997500a
eric <smileeric0414@gmail.com>
2024-04-29 00:03:21 +0800

    update powerset like problems

---
+++ leetcode/39. Combination Sum/a.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        // only one recursion, with TC of O(2^N)
+        vector<vector<int>> ans;
+        int n = candidates.size();
+        vector<int> combination;
+        tryAllCombination(ans, candidates, 0, target, combination);
+        return ans;
+    }
+
+    void tryAllCombination(vector<vector<int>>& ans, vector<int>& candidates, int currIdx, int target, vector<int>& combination) {
+        if (target < 0) return;
+        if (target == 0) {
+            ans.push_back(combination);
+            return;
+        }
+        for (int i = currIdx; i < candidates.size(); i++) {
+            int num = candidates[i];
+            combination.push_back(num);
+            tryAllCombination(ans, candidates, i, target - num, combination); // try to pick next num
+            combination.pop_back();
+        }
+    }
+};
+
+class Solution {
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        // two recursion, with TC of O(2^N) and SC of O(N)
+        vector<vector<int>> ans;
+        int n = candidates.size();
+        vector<int> combination;
+        tryAllCombination(ans, candidates, 0, target, combination);
+        return ans;
+    }
+
+    void tryAllCombination(vector<vector<int>>& ans, vector<int>& candidates, int i, int target, vector<int>& combination) {
+        if (target < 0 || i == candidates.size()) return;
+        if (target == 0) {
+            ans.push_back(combination);
+            return;
+        }
+        int num = candidates[i];
+        combination.push_back(num);
+        tryAllCombination(ans, candidates, i, target - num, combination); // try if we can pick one more num 
+        combination.pop_back();
+        tryAllCombination(ans, candidates, i + 1, target, combination); // try to pick next num
+    }
+};
