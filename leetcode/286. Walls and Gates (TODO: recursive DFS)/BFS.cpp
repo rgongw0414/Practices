780f07bca451a250e64c9bd1a8114a4c2552c66b
eric <smileeric0414@gmail.com>
2024-05-07 19:03:11 +0800

    leetcode 286: add comments for BFS

--- leetcode/286. Walls and Gates (TODO: recursive DFS)/BFS.cpp
+++ leetcode/286. Walls and Gates (TODO: recursive DFS)/BFS.cpp
@@ -14,7 +14,8 @@ public:
     void islandsAndTreasure(vector<vector<int>>& grid) {
         // Time:  O(m x n)
         // Space: O(m x n)
-        // BFS: It is a little tricky that it first pushes all land cells into the queue.
+        // BFS trick: It starts from all the chests (push them into queue), and starts traversing from them, 
+        //          resulting in for each chest, it takes one step until the sea, or a land which has been vistid before is reached.
         // If a land cell cannot reach a treasure chest than the value should remain INF_MAX.
         queue<pair<int, int>> q;
         for (int i = 0; i < grid.size(); i++) {
@@ -35,7 +36,8 @@ public:
                 int jStepped = j + coor.second;
                 if (iStepped < 0 || iStepped >= grid.size() || 
                         jStepped < 0 || jStepped >= grid[0].size() || grid[iStepped][jStepped] != INT_MAX) {
-                    // out-of-bound, not a land, or has been stepped in before.
+                    // out-of-bound, not a land, or has been stepped in before. 
+                    // Why skip the land cell? Because each chest takes turn taking one step, which garantees the shortest path to chests.
                     continue;
                 }
                 grid[iStepped][jStepped] = grid[i][j] + 1;
