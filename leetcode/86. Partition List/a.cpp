aaaa6a1f6b1a7e4515646b5df7a2eca658dcdb23
rgongw <smileeric0414@gmail.com>
2025-03-25 01:41:50 +0800

    86. Partition List: add brute-force method

--- leetcode/86. Partition List/a.cpp
+++ leetcode/86. Partition List/a.cpp
@@ -31,6 +31,8 @@ d 1->1->nullptr<-r
 class Solution {
 public:
     ListNode* partition(ListNode* head, int x) {
+        // TC:O(n), SC:O(1)
+        // Two Pointers
         ListNode* l_dummy = new ListNode(777); // l_dummy->next is the head of the left partition
         ListNode* r_dummy = new ListNode(888); // r_dummy->next is the head of the right partition
         ListNode* l_prev = l_dummy, *r_prev = r_dummy, *curr = head;
@@ -50,5 +52,49 @@ public:
         r_prev->next = nullptr; // end the right partition
         l_prev->next = r_dummy->next; // connect the left partition to the right partition
         return l_dummy->next;
+    }
+};
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        // TC:O(n), SC:O(n)
+        // Brute Force
+        if (!head) return nullptr;
+        
+        vector<int> less, greater; // store the values of the nodes
+        ListNode* cur = head;
+
+        while (cur) {
+            if (cur->val < x) {
+                less.push_back(cur->val);
+            } else {
+                greater.push_back(cur->val);
+            }
+            cur = cur->next;
+        }
+
+        cur = head;
+        for (int val : less) { // update the values of the nodes
+            cur->val = val;
+            cur = cur->next;
+        }
+
+        for (int val : greater) {
+            cur->val = val;
+            cur = cur->next;
+        }
+
+        return head;
     }
 };
