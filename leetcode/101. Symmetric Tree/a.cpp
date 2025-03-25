211ff42b253fdf1ee7f5636764e99c7ee8be107b
rgongw <smileeric0414@gmail.com>
2025-03-25 15:01:53 +0800

    udpate 101

--- leetcode/101. Symmetric Tree/a.cpp
+++ leetcode/101. Symmetric Tree/a.cpp
@@ -60,3 +60,37 @@ public:
         return true;
     }
 };
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        // BFS
+        // TC:O(n), SC:O(n)
+        queue<pair<TreeNode*, TreeNode*>> queue;
+        queue.push({root->left, root->right}); // push the symmertric nodes
+        while (!queue.empty()) {
+            for (int i = queue.size(); i > 0; i--) {   
+                auto [left, right] = queue.front();
+                queue.pop();
+                if (!left && !right) continue;
+                if (!left || !right || left->val != right->val) {
+                    return false;
+                }
+                queue.push({left->left, right->right});
+                queue.push({left->right, right->left});
+            }
+        }
+        return true;
+    }
+};
