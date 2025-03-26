8179735d392f28a0ef3c819a82cfeffb6972e6f6
rgongw <smileeric0414@gmail.com>
2025-03-27 02:27:39 +0800

    112. Path Sum: Iterative DFS

--- leetcode/112. Path Sum/a.cpp
+++ leetcode/112. Path Sum/a.cpp
@@ -20,3 +20,30 @@ public:
         return hasPathSum(root->left, targetSum - root->val) || hasPathSum(root->right, targetSum - root->val);
     }
 };
+
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if (!root) return false;
+        queue<pair<TreeNode*, int>> q;
+        q.push({root, root->val});
+        while (!q.empty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                auto iter = q.front();
+                q.pop();
+                TreeNode* node = iter.first;
+                if (node->left) {
+                    q.push({node->left, iter.second + node->left->val});
+                }
+                if (node->right) {
+                    q.push({node->right, iter.second + node->right->val});
+                }
+                if (!node->left && !node->right && iter.second == targetSum) {
+                    return true;
+                }
+            }           
+        }
+        return false;
+    }
+};
