de21a1ce7eed39c832296bb9e5a8d0ce16c65a24
rgongw <smileeric0414@gmail.com>
2025-03-28 00:52:13 +0800

    399. Evaluate Division: add Recursive DFS

--- leetcode/399. Evaluate Division/a.cpp
+++ leetcode/399. Evaluate Division/a.cpp
@@ -20,7 +20,7 @@ public:
 
         vector<double> res;
         // For each query, do BFS
-        for (const string& query : queries) { 
+        for (const auto& query : queries) { 
             string src = query[0];
             string target = query[1];
             res.push_back(bfs(src, target, adj_list));
@@ -33,13 +33,15 @@ private:
         if (!adj_list.count(src) || !adj_list.count(target)) {
             return -1.0;
         }
-        // src->...->target
+        // src->node->...->target
         queue<pair<string, double>> q; // for each node, we store the node and the weight from src to this node
         unordered_set<string> visited;
         q.emplace(src, 1.0);
         visited.insert(src);
 
         while (!q.empty()) {
+            // In the 1st iter: node = src, weight = src / src = 1.0; 
+            // in next iter, node is a neighbor of src, weight is the product of all weights from src to node
             auto [node, weight] = q.front(); 
             q.pop();
 
@@ -54,7 +56,53 @@ private:
                 }
             }
         }
+        return -1.0; // return -1.0 if no path found
+    }
+};
 
+class Solution {
+public:
+    vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
+        // Recursive DFS
+        // TC: O(V + E), SC: O(V + E)
+        // Build the graph
+        unordered_map<string, vector<pair<string, double>>> adj_list; // Map a -> list of [b, a/b]
+        for (int i = 0; i < equations.size(); i++) {
+            string a = equations[i][0];
+            string b = equations[i][1];
+            adj_list[a].emplace_back(b, values[i]);
+            adj_list[b].emplace_back(a, 1.0 / values[i]);
+        }
+
+        vector<double> res;
+        // For each query, do DFS
+        for (const auto& query : queries) { 
+            string src = query[0];
+            string target = query[1];
+            unordered_set<string> visited;
+            res.push_back(dfs(src, target, adj_list, visited));
+        }
+        return res;
+    }
+
+private:
+    double dfs(const string& src, const string& target, unordered_map<string, vector<pair<string, double>>>& adj_list, unordered_set<string>& visited) {
+        if (!adj_list.count(src) || !adj_list.count(target)) {
+            return -1.0;
+        }
+        if (src == target) {
+            return 1.0;
+        }
+        visited.insert(src);
+
+        for (const auto& [nei, neiWeight] : adj_list[src]) { // push all neighbors of src
+            if (!visited.count(nei)) {
+                double weight = dfs(nei, target, adj_list, visited); // corrected 'visite' to 'visited'
+                if (weight != -1.0) {
+                    return weight * neiWeight;
+                }
+            }
+        }
         return -1.0; // return -1.0 if no path found
     }
 };
