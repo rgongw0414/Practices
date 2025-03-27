33c768701a9d3a4392a2f7135f6f72d09d27695b
rgongw <smileeric0414@gmail.com>
2025-03-27 21:58:01 +0800

    399. Evaluate Division

---
+++ leetcode/399. Evaluate Division/a.cpp
@@ -0,0 +1,60 @@
+/*
+equations = [["a","b"],
+             ["b","c"]]
+equations[0]: ["a","b"]
+*/
+
+class Solution {
+public:
+    vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
+        // BFS
+        // TC: O(V + E), SC: O(V + E)
+        // Build the graph
+        unordered_map<string, vector<pair<string, double>>> adj_list; // Map a -> list of [b, a/b]
+        for (int i = 0; i < equations.size(); i++) {
+            string a = equations[i][0];
+            string b = equations[i][1];
+            adj_list[a].emplace_back(b, values[i]);
+            adj_list[b].emplace_back(a, 1.0 / values[i]);
+        }
+
+        vector<double> res;
+        // For each query, do BFS
+        for (const string& query : queries) { 
+            string src = query[0];
+            string target = query[1];
+            res.push_back(bfs(src, target, adj_list));
+        }
+        return res;
+    }
+
+private:
+    double bfs(const string& src, const string& target, unordered_map<string, vector<pair<string, double>>>& adj_list) {
+        if (!adj_list.count(src) || !adj_list.count(target)) {
+            return -1.0;
+        }
+        // src->...->target
+        queue<pair<string, double>> q; // for each node, we store the node and the weight from src to this node
+        unordered_set<string> visited;
+        q.emplace(src, 1.0);
+        visited.insert(src);
+
+        while (!q.empty()) {
+            auto [node, weight] = q.front(); 
+            q.pop();
+
+            if (node == target) {
+                return weight;
+            }
+
+            for (const auto& [nei, neiWeight] : adj_list[node]) { // push all neighbors of node
+                if (!visited.count(nei)) {
+                    q.emplace(nei, weight * neiWeight); // weight from src to nei = weight from src to node * weight from node to nei
+                    visited.insert(nei);
+                }
+            }
+        }
+
+        return -1.0; // return -1.0 if no path found
+    }
+};
