5d19c013822cd53d199a0484bba715d6d41e6139
rgongw <smileeric0414@gmail.com>
2025-03-30 01:35:46 +0800

    77. Combinations: Add bit manipulation

--- leetcode/77. Combinations/a.cpp
+++ leetcode/77. Combinations/a.cpp
@@ -39,7 +39,7 @@ public:
         for (int mask = 0; mask < (1 << n); ++mask) {
             vector<int> comb;
             for (int bit = 0; bit < n; ++bit) {
-                if (mask & (1 << bit)) {
+                if (mask & (1 << bit)) { // Check each bit in the mask
                     comb.push_back(bit + 1);
                 }
             }
