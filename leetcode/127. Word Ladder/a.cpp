07816d62303364738c16b90f0e4c954075465547
rgongw <smileeric0414@gmail.com>
2025-03-29 23:55:30 +0800

    127. Word Ladder: Revise TC & SC, where #edge=C(N, 2)

--- leetcode/127. Word Ladder/a.cpp
+++ leetcode/127. Word Ladder/a.cpp
@@ -1,6 +1,45 @@
+/*
+Input: beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log","cog"]
+Output: 5
+Explanation: One shortest transformation sequence is "hit" -> "hot" -> "dot" -> "dog" -> cog", which is 5 words long.
+     front
+    q: hit
+        ait (skipped) bit (skipped) ... zit (skipped)
+        hat (skipped) hbt (skipped) ... hot (added), hpt (skipped) ... 
+    q: hot
+        dot (added) lot (added) 
+    q: dot lot
+        dog (added) log (added)
+    q: lot dog log
+    ...
+    q: log
+        cog (endWord found)
+
+       hit
+        |
+       hot
+       / \
+     dot lot
+    /...|...\
+       dog  log
+              \
+              cog
+
+Worst case: Complete graph, where |E| = |V|*(|V| - 1) / 2, where |E|= number of edges, |V| = number of vertices
+    |E| = O(N^2), where N = wordList.size()
+    |V| = O(N)
+    TC = O(M * N^2), where M = wordList[i].size()
+*/
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        // BFS
+        // The neighbor of a word is created when traversing the word by changing one letter at a time.
+        // TC: O(M * N^2)
+        //     N: wordList.size(),
+        //     M: wordList[i].size()
+        //     26: number of possible mutations (a-z)
+        // SC: O(N)
         unordered_set<string> words(wordList.begin(), wordList.end());
         if (!words.count(endWord)) return 0;
         queue<string> q;
