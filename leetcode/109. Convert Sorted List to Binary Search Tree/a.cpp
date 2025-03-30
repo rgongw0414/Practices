9249b41ce662b999797f983f6c4639bcf04c40f3
rgongw <smileeric0414@gmail.com>
2025-03-30 23:47:56 +0800

    109. Convert Sorted List to Binary Search Tree: Merge sort

--- leetcode/109. Convert Sorted List to Binary Search Tree/a.cpp
+++ leetcode/109. Convert Sorted List to Binary Search Tree/a.cpp
@@ -8,17 +8,54 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        // Merge sort
+        // TC: O(N*logN)
+        // SC: O(logN) for recursion stack
+        if (!head || !head->next) return head;
+        ListNode* left_head = head;
+        ListNode* mid = getMid(head); // mid is the tail of the left half
+        ListNode* right_head = mid->next;
+        mid->next = nullptr;
+
+        left_head = sortList(left_head);
+        right_head = sortList(right_head);
+        return merge(left_head, right_head);
+    }
+
+private:
+    ListNode* getMid(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+
+    ListNode* merge(ListNode* list1, ListNode* list2) {
+        ListNode dummy(0); // for easier handling of the head
+        ListNode* curr = &dummy; 
+        while (list1 && list2) {
+            if (list1->val < list2->val) {
+                curr->next = list1;
+                list1 = list1->next;
+            } 
+            else {
+                curr->next = list2;
+                list2 = list2->next;
+            }
+            curr = curr->next;  
+        }
+        if (list1) curr->next = list1;
+        if (list2) curr->next = list2;
+        return dummy.next;
+    }
+};
+
 class Solution {
 public:
     TreeNode* sortedListToBST(ListNode* head) {
@@ -73,5 +110,32 @@ public:
         root->left = DFS(l, mid - 1, head, n);
         root->right = DFS(mid + 1, r, head, n);
         return root;
+    }
+};
+
+class Solution {
+public:
+    TreeNode* sortedListToBST(ListNode* head) {
+        // Slow and fast pointer: For finding the mid node faster
+        // TC: O(N*logN)
+        // SC: O(logN) for recursion stack
+        if (head == nullptr) return nullptr;
+        return convertToBST(head, nullptr);
+    }
+
+    TreeNode* convertToBST(ListNode* start, ListNode* end) {
+        if (start == end) return nullptr;
+        ListNode* slow = start;
+        ListNode* fast = start;
+
+        while (fast != end && fast->next != end) { // Only half of the sublist is traversed
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        TreeNode* head = new TreeNode(slow->val);
+        head->left = convertToBST(start, slow);
+        head->right = convertToBST(slow->next, end);
+        return head;
     }
 };
