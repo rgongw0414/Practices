6cc69b1e992cb0281d2b2cc1947cf20c48ee999f
rgongw <smileeric0414@gmail.com>
2025-04-18 02:15:28 +0800

    72. Edit Distance: Bottom-up 1-d DP, TC: O(m*n), SC: O(min(m, n))

--- leetcode/72. Edit Distance/a.cpp
+++ leetcode/72. Edit Distance/a.cpp
@@ -21,3 +21,33 @@ public:
         return dp[0][0];
     }
 };
+
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        // Bottom-up 1-d DP, TC: O(m*n), SC: O(min(m, n))
+        int m = word1.size(), n = word2.size();
+        if (m < n) {
+            swap(m, n);
+            swap(word1, word2);
+        }
+
+        vector<int> dp(n + 1, 0);
+        for (int i = 0; i <= n; i++) dp[i] = n - i;
+
+        for (int i = m - 1; i >= 0; i--) {
+            int nextDp = dp[n];
+            dp[n] = m - i;
+            for (int j = n - 1; j >= 0; j--) {
+                int temp = dp[j];
+                if (word1[i] == word2[j]) {
+                    dp[j] = nextDp;
+                } else {
+                    dp[j] = 1 + min({dp[j], dp[j + 1], nextDp});
+                }
+                nextDp = temp;
+            }
+        }
+        return dp[0];
+    }
+};
