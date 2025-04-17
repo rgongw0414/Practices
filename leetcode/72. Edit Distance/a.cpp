08f6d37e72ae5153cf4db37d906a06101aef71c7
rgongw <smileeric0414@gmail.com>
2025-04-18 01:28:29 +0800

    72. Edit Distance: Top-down DP, TC: O(m*n), SC: O(m*n)

---
+++ leetcode/72. Edit Distance/a.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        // Top-down DP, TC: O(m*n), SC: O(m*n)
+        // dp[i][j] = min distance of word1[0..i-1] and word2[0..j-1]
+        int m = word1.size(), n = word2.size();
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+        for (int i = 0; i < m; i++) dp[i][n] = m - i; // #insertion needed
+        for (int i = 0; i < n; i++) dp[m][i] = n - i;
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                if (word1[i] == word2[j]) {
+                    dp[i][j] = dp[i+1][j+1];
+                }
+                else {
+                    dp[i][j] = 1 + min(dp[i+1][j+1], min(dp[i+1][j], dp[i][j+1]));
+                }
+            }
+        }
+        return dp[0][0];
+    }
+};
